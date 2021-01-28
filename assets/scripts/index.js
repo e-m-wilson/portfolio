b2e645a47f1b16e763bdeaaf91adf79ad18f665c
Ethan Wilson <ethanwilson1991@gmail.com>
2021-01-27 20:07:56 -0600

    splash anim done, misc styling

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,12 @@
 "use strict";
 
+
+$("#start").click(openPage);
+
+function openPage () {
+    $("#splash").addClass("removed");
+    $("#splash").on("animationend", () => {
+        $("#splash").hide();
+    });
+}
+
