6b99ba3a0209c4c81245bfff59ba140e229ba6de
Ethan Wilson <ethanwilson1991@gmail.com>
2021-02-04 17:43:22 -0600

    v1

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,9 @@ document.querySelector('#start').addEventListener('click', openPage);
 
 function openPage () {
     document.querySelector('#splash').className = 'removed';
-    document.querySelector('#splash').addEventListener('animationend', () => 
-    document.querySelector('#splash').remove());
+    document.querySelector('#splash').addEventListener('animationend', () => {
+        document.querySelector('#splash').remove();
+        document.querySelector('#navBar').className = 'fixed';
+    })
 }
 
